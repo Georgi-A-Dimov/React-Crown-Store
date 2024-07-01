8ac29976e107f08280b27732827f95554031ce69
Georgi-A-Dimov <122820016+Georgi-A-Dimov@users.noreply.github.com>
2024-07-02 02:37:59 +0300

    Added shop cart icon to navigation bar.
    Added cart dropdown.

--- my-app/src/routes/navigation/navigation.component.jsx
+++ my-app/src/routes/navigation/navigation.component.jsx
@@ -5,6 +5,8 @@ import CrownLogo from '../../assets/crown.svg?react';
 import './navigation.styles.scss';
 import { UserContext } from "../../contexts/user.context";
 import { signOutUser } from "../../utils/firebase.utils";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
 const Navigation = () => {
     const { currentUser } = useContext(UserContext);
@@ -30,7 +32,10 @@ const Navigation = () => {
                             SIGN IN
                         </Link>)
                     }
+
+                    <CartIcon/>
                 </div>
+                <CartDropdown/>
             </div>
 
             <Outlet />
