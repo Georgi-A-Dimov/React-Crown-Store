7a71afed68f3b7b018a62a32b7db88ead2a9e7f3
Georgi-A-Dimov <122820016+Georgi-A-Dimov@users.noreply.github.com>
2024-07-13 20:08:12 +0300

    Updated cart
    Now shows the amount of products in the cart. Added a new route checkout, displaying all the items added to the cart.

--- my-app/src/components/cart-dropdown/cart-dropdown.component.jsx
+++ my-app/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,18 +1,29 @@
 import "./cart-dropdown.styles.scss";
+
 import Button from "../button/button.component"
 import CartItem from "../cart-item/cart-item.component";
-import { useContext } from "react";
 import { CartContext } from "../../contexts/cart.context";
+
+import { useContext } from "react";
+import { useNavigate } from "react-router-dom";
 
 const CartDropdown = () => {
     const { cartItems } = useContext(CartContext);
 
+    const navigate = useNavigate();
+    const goToCheckoutHandler = () => {
+        navigate('/checkout');
+    };
+
     return (
         <div className="cart-dropdown-container">
-            <div className="cart-items">
-                {cartItems.map((item) => (<CartItem key={item.id} cartItem={item} />))}
-            </div>
-            <Button>GO TO CHECKOUT</Button>
+            {cartItems.length > 0
+                ? <div className="cart-items">
+                    {cartItems.map((item) => (<CartItem key={item.id} cartItem={item} />))}
+                  </div>
+                : <span className="empty-message">Your cart is empty</span>
+            }
+            <Button onClick={goToCheckoutHandler}>GO TO CHECKOUT</Button>
         </div>
     );
 };
